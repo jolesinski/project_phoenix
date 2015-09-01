8385e83a86b870cdc349b8c7c728b82b4f7e4f81
Michal Zygmunt <miczyg13@gmail.com>
2015-09-01 23:32:38 +0200

    Course and distance based on GPS.

--- core_rover/server/subsystems/location/regulator.py
+++ core_rover/server/subsystems/location/regulator.py
@@ -1,6 +1,8 @@
 __author__ = 'miczyg'
 
 import math
+from math import cos
+from math import sin
 from server.subsystems.chassis.chassis import Chassis
 from server.subsystems.location.basegps import GpsModule
 
@@ -25,3 +27,42 @@ class Regulator(object):
         long_diff = self.destination[1] - current_position[1]
         distance = math.hypot(lat_diff, long_diff)
         pass
+
+    def distance_to(self, position, destination):
+        delta = math.radians( position[1] - destination[1] )
+        sin_delta = sin(delta)
+        cos_delta = cos(delta)
+        pos_lati = math.radians(position[0])
+        dest_lati = math.radians(destination[0])
+
+        delta = ( cos(pos_lati) * sin(dest_lati) ) \
+                - ( sin(pos_lati) * cos(dest_lati) * cos_delta )
+
+        delta = math.sqrt(delta)
+        delta += math.sqrt( cos(dest_lati) * sin_delta )
+
+        delta = math.sqrt(delta)
+
+        denom = ( sin(pos_lati) * sin(dest_lati) ) \
+                + (cos(pos_lati) * cos(dest_lati) * cos_delta)
+
+        delta = math.atan2(delta, denom)
+
+        distance = delta * 6372795 #distance in meters
+        return distance
+
+    def course_to(self, position, destination):
+        diff_long = math.radians( position[1] - destination[1] )
+        pos_lati = math.radians(position[0])
+        dest_lati = math.radians(destination[0])
+
+        course_dump1 = sin(diff_long) - cos(dest_lati)
+        course_dump2 = sin(pos_lati) * cos(dest_lati) * cos(diff_long)
+        course_dump1 = cos(pos_lati) * sin(dest_lati) - course_dump2
+
+        course = math.atan2(course_dump1, course_dump2)
+
+        if course < 0:
+            course += 2 * math.PI
+
+        return course
